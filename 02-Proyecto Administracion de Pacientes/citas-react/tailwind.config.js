40777ffe35f261bdbdfc5cf0c2bc08e020b25563
joseDG <josethp1@gmail.com>
2023-06-09 23:12:28 -0500

    Proyecto administracion de pacientes

---
+++ 02-Proyecto Administracion de Pacientes/citas-react/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+//para el deploy "index.html", "./src/**/*.jsx"
+export default {
+  content: ["index.html", "./src/**/*.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
