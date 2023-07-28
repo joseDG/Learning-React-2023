dd2c5268c6e689d1b1052b34a29f9ffff23f0fb2
JoseDG <josethp1@gmail.com>
2023-07-28 10:07:13 -0500

    actualizando routes
    se elimino las routes porque solo son de pruebas

--- 15-UP-TASK-MERN/backend/routes/usuarioRoutes.js
+++ 15-UP-TASK-MERN/backend/routes/usuarioRoutes.js
@@ -2,12 +2,9 @@ import express from "express";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send("DESDE API/USUARIOS");
-})
-
-router.post("/", (req, res) => {
+router.get("/", (req, res) => {
   res.send("DESDE API/USUARIOS");
 });
+
 
 export default router;
