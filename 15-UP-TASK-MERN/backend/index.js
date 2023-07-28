133eb3b8abb0a0830dc9cf237baf54dbb01875fe
JoseDG <josethp1@gmail.com>
2023-07-28 09:56:39 -0500

    agregando routes
    esta parte agregamos routes

--- 15-UP-TASK-MERN/backend/index.js
+++ 15-UP-TASK-MERN/backend/index.js
@@ -2,13 +2,17 @@ import express from "express";
 import conectarDB from "./config/db.js";
 import dotenv from "dotenv";
 
-dotenv.config();
+import usuarioRoutes from './routes/usuarioRoutes.js'
 
 const app = express();
+
+dotenv.config();
 conectarDB();
 
-const PORT = process.env.PORT || 4000;
+//Routing
+app.use("/api/usuarios", usuarioRoutes);
 
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
