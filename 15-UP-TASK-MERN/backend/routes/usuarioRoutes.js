bfce797d5e7016df54674499106b55bf9c6f4e93
JoseDG <josethp1@gmail.com>
2023-07-31 13:03:03 -0500

    comprobando-Token
    esta funcion me permite comprobar el token

--- 15-UP-TASK-MERN/backend/routes/usuarioRoutes.js
+++ 15-UP-TASK-MERN/backend/routes/usuarioRoutes.js
@@ -4,6 +4,7 @@ import {
   autenticar,
   confirmar,
   olvidePassword,
+  comprobarToken,
 } from "../controllers/usuarioController.js";
 
 const router = express.Router();
@@ -13,6 +14,7 @@ router.post('/', registrar);
 router.post('/login', autenticar)
 router.get('/confirmar/:token', confirmar)
 router.post('/olvide-password', olvidePassword)
+router.get("/olvide-password/:token", comprobarToken)
 
 
 export default router;
