aadedc7f4159602b139c726e5de660b2b4448f95
JoseDG <josethp1@gmail.com>
2023-07-31 12:54:25 -0500

    obtener un nuevo token
    olvide contraena se debe obtener un nuevo token

--- 15-UP-TASK-MERN/backend/controllers/usuarioController.js
+++ 15-UP-TASK-MERN/backend/controllers/usuarioController.js
@@ -74,5 +74,21 @@ const confirmar = async (req, res) => {
 
 };
 
+const olvidePassword = async (req, res) => {
+  const {email} = req.body;
+  const usuario = await Usuario.findOne({ email })
+  if(!usuario){
+    const error = new Error("El Usuario no existe");
+    return res.status(404).json({ msg: error.message });
+  }
 
-export { registrar, autenticar, confirmar }
+  try{
+    usuario.token = generarId();
+    await usuario.save()
+    res.json({msg: "Hemos enviado un email con las instrucciones"})
+  }catch(error){
+    console.log(error)
+  }
+}
+
+export { registrar, autenticar, confirmar, olvidePassword };
--- 15-UP-TASK-MERN/backend/routes/usuarioRoutes.js
+++ 15-UP-TASK-MERN/backend/routes/usuarioRoutes.js
@@ -1,5 +1,10 @@
 import express from "express";
-import {registrar, autenticar, confirmar} from '../controllers/usuarioController.js'
+import {
+  registrar,
+  autenticar,
+  confirmar,
+  olvidePassword,
+} from "../controllers/usuarioController.js";
 
 const router = express.Router();
 
@@ -7,5 +12,7 @@ const router = express.Router();
 router.post('/', registrar);
 router.post('/login', autenticar)
 router.get('/confirmar/:token', confirmar)
+router.post('/olvide-password', olvidePassword)
+
 
 export default router;
