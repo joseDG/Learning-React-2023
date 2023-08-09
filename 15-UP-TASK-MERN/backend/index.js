ce53315dab5b4c44d7a1ccdb5d36460bd934c319
JoseDG <josethp1@gmail.com>
2023-08-09 11:51:16 -0500

    Proyectos
    creacion de rutas controladores modelos para el modulo de proyectos

--- 15-UP-TASK-MERN/backend/index.js
+++ 15-UP-TASK-MERN/backend/index.js
@@ -3,6 +3,7 @@ import conectarDB from "./config/db.js";
 import dotenv from "dotenv";
 
 import usuarioRoutes from './routes/usuarioRoutes.js'
+import proyectoRoutes from "./routes/proyectoRoutes.js"
 
 const app = express();
 //procesa la informacion tipo json
@@ -13,6 +14,7 @@ conectarDB();
 
 //Routing
 app.use("/api/usuarios", usuarioRoutes);
+app.use("/api/proyectos", proyectoRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
