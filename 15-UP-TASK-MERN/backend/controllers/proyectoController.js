2b42c85dee5a94b02e0beb218dbf9be02004d668
JoseDG <josethp1@gmail.com>
2023-08-09 13:52:42 -0500

    Actualizar Proyecto

--- 15-UP-TASK-MERN/backend/controllers/proyectoController.js
+++ 15-UP-TASK-MERN/backend/controllers/proyectoController.js
@@ -9,30 +9,29 @@ const obtenerProyectos = async (req, res) => {
 
 //Crear Proyectos
 const nuevoProyecto = async (req, res) => {
-  const proyecto = new Proyecto(req.body)
-  proyecto.creador = req.usuario._id
+  const proyecto = new Proyecto(req.body);
+  proyecto.creador = req.usuario._id;
 
   try {
-    const proyectoAlmacenado = await proyecto.save()
+    const proyectoAlmacenado = await proyecto.save();
     res.json(proyectoAlmacenado);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 
 //Obtener Proyectos por ID
 const obtenerProyecto = async (req, res) => {
-
-  const {id} = req.params;
+  const { id } = req.params;
 
   const proyecto = await Proyecto.findById(id);
 
-  if(!proyecto){
+  if (!proyecto) {
     const error = new Error("No Encontrado");
-    return res.status(404).json({msg: error.message });
+    return res.status(404).json({ msg: error.message });
   }
 
-  if(proyecto.creador.toString() !== req.usuario._id.toString()){
+  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
     const error = new Error("Accion No valida");
     return res.status(401).json({ msg: error.message });
   }
@@ -40,12 +39,38 @@ const obtenerProyecto = async (req, res) => {
   res.json(proyecto);
 };
 
+//Editar Proyecto
 const editarProyecto = async (req, res) => {
+  const { id } = req.params;
 
-  
+  const proyecto = await Proyecto.findById(id);
+
+  if (!proyecto) {
+    const error = new Error("No Encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Accion No valida");
+    return res.status(401).json({ msg: error.message });
+  }
+
+  proyecto.nombre = req.body.nombre || proyecto.nombre;
+  proyecto.descripcion = req.body.descripcion || proyecto.descripcion;
+  proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega;
+  proyecto.cliente = req.body.cliente || proyecto.cliente;
+
+  try{
+    const proyectoAlmacenado = await proyecto.save()
+    res.json(proyectoAlmacenado)
+  }catch(error){
+    console.log(error)
+  }
 };
 
-const eliminarProyecto = async (req, res) => {};
+const eliminarProyecto = async (req, res) => {
+  
+};
 
 const agregarColaborador = async (req, res) => {};
 
@@ -61,5 +86,5 @@ export {
   eliminarProyecto,
   agregarColaborador,
   eliminarColaborador,
-  obtenerTareas
+  obtenerTareas,
 };
