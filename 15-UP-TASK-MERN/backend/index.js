0377c1c9e508af37fda23e74434c89737c86e4a9
JoseDG <josethp1@gmail.com>
2023-08-09 13:22:06 -0500

    Nuevo Proyectos

--- 15-UP-TASK-MERN/backend/index.js
+++ 15-UP-TASK-MERN/backend/index.js
@@ -16,7 +16,7 @@ conectarDB();
 app.use("/api/usuarios", usuarioRoutes);
 app.use("/api/proyectos", proyectoRoutes);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
