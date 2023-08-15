9aeb5c07b9e1b11620488bff95f93035905fbdc8
JoseDG <josethp1@gmail.com>
2023-08-15 13:18:42 -0500

    add proyecto

--- 15-UP-TASK-MERN/frontend/src/App.jsx
+++ 15-UP-TASK-MERN/frontend/src/App.jsx
@@ -10,6 +10,7 @@ import NuevoPassword from './paginas/NuevoPassword'
 import ConfirmarCuenta from './paginas/ConfirmarCuenta'
 import Proyectos from "./paginas/Proyectos";
 import NuevoProyecto from "./paginas/NuevoProyecto";
+import Proyecto from "./paginas/Proyecto";
 
 import { AuthProvider } from "./context/AuthProvider";
 import { ProyectosProvider } from "./context/ProyectosProvider";
