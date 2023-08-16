b4f682749bb72ffb67b3426c4ec13bb9ed7ec8e0
JoseDG <josethp1@gmail.com>
2023-08-16 10:09:00 -0500

    actualizando proyecto controller

--- 15-UP-TASK-MERN/backend/controllers/proyectoController.js
+++ 15-UP-TASK-MERN/backend/controllers/proyectoController.js
@@ -3,7 +3,7 @@ import Tarea from "../models/Tarea.js";
 
 //Obtener Proyectos User Loguados
 const obtenerProyectos = async (req, res) => {
-  const proyectos = await Proyecto.find().where("creador").equals(req.usuario);
+  const proyectos = await Proyecto.find().where("creador").equals(req.usuario).select("-tareas");
 
   res.json(proyectos);
 };
@@ -25,7 +25,7 @@ const nuevoProyecto = async (req, res) => {
 const obtenerProyecto = async (req, res) => {
   const { id } = req.params;
 
-  const proyecto = await Proyecto.findById(id);
+  const proyecto = await Proyecto.findById(id).populate('tareas');
 
   if (!proyecto) {
     const error = new Error("No Encontrado");
